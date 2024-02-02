63b65b0baf012433d2efc636a9ab5a3cff232067
BlinkTim <zetteronid@gmail.com>
2024-02-02 15:30:19 +0100

    try to fix last two issues

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
 import shlex
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +19,5 @@ def execute_command_safe():
         return f"Error executing command: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    # Ändere host auf '127.0.0.1' anstatt '0.0.0.0'
+    app.run(debug=False, host='127.0.0.1', port=5000)
